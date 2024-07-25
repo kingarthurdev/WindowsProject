d8a19d0c97a600622ec43d0a5c5b2709a755b30b
Chen <arthur.chen@emerson.com>
2024-07-25 14:51:07 -0500

    asdf

--- WindowsService1/plainconsolefortesting/Program.cs
+++ WindowsService1/plainconsolefortesting/Program.cs
@@ -32,7 +32,8 @@ namespace dotNetService
         public static void listen()
         {
             ProcessContent.WriteToFile("Listening on port 12000");
-            Console.WriteLine("Listening on port 12000, forwarding to port " + proxyListeningForAckPort);
+            Console.WriteLine("Listening on port 12000, sending responses to port " + proxyListeningForAckPort);
+            Console.WriteLine("What port would you like to send response messages to? (default = 1543, proxy = 1544)");
             UdpClient listener = new UdpClient(12000);
             IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 12000);
 
--- WindowsService1/plainconsolefortesting/obj/Debug/net8.0/UnencryptedDataSender.AssemblyInfo.cs
+++ WindowsService1/plainconsolefortesting/obj/Debug/net8.0/UnencryptedDataSender.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnencryptedDataSender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ba4aa97b6a9fb4a06719b1d502e0ebb7be3bd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d6a4b2505c2a21f7a61655a1ae36445d02cfc45")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnencryptedDataSender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnencryptedDataSender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
